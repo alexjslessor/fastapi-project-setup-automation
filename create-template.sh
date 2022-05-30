483c1e3ee4935f7f48245382c027e5f75566aa1b
Alexander Slessor <alexjslessor@gmail.com.com>
2022-05-30 11:05:10 -0400

    updated readme

--- create-template.sh
+++ create-template.sh
@@ -257,16 +257,13 @@ uvicorn[standard]
 python-multipart
 httpx
 
-
 # testing related
-#asgi-lifespan
-#pytest-asyncio
-#pytest-cov
-
+asgi-lifespan
+pytest-asyncio
+pytest-cov
 
 # mongodb related
 motor
-
 
 # celery worker related
 #celery
@@ -320,8 +317,8 @@ create_requirements_txt
 create_env_files
 }
 
-main_init
-# rm -rf $base_dir $base_dir_tests $env_folder __pycache__ && rm $reqs $main $startup $dockerfile $setup_cfg $gitignore $dockerignore
+# main_init
+rm -rf $base_dir $base_dir_tests $env_folder __pycache__ && rm $reqs $main $startup $dockerfile $setup_cfg $gitignore $dockerignore
 
 
 # print_eof() {
